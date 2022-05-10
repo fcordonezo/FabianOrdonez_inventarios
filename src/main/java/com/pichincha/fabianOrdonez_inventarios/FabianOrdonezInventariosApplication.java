a264e466d168079c730ea3619ba8b1557aa97e67
Fabian Ordonez <fabian.ordonez@devsu.com>
2022-05-10 14:03:41 -0500

    chore: add project template

---
+++ src/main/java/com/pichincha/fabianOrdonez_inventarios/FabianOrdonezInventariosApplication.java
@@ -0,0 +1,13 @@
+package com.pichincha.fabianOrdonez_inventarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabianOrdonezInventariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabianOrdonezInventariosApplication.class, args);
+	}
+
+}
