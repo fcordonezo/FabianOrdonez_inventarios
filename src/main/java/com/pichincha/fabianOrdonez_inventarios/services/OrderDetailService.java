4147d4905b57128dcd9093b7731b8cd4aacc1589
Fabian Ordonez <fabian.ordonez@devsu.com>
2022-05-11 22:09:36 -0500

    feat(services): add orderDetail service interface

---
+++ src/main/java/com/pichincha/fabianOrdonez_inventarios/services/OrderDetailService.java
@@ -0,0 +1,7 @@
+package com.pichincha.fabianOrdonez_inventarios.services;
+
+import com.pichincha.fabianOrdonez_inventarios.entities.OrderDetail;
+
+public interface OrderDetailService {
+  public void addOrderDetail(OrderDetail orderDetail);
+}
