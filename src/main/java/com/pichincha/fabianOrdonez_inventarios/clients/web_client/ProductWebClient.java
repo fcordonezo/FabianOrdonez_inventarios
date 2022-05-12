89d649029a1c5e89baf684236013c7ac150710ff
Fabian Ordonez <fabian.ordonez@devsu.com>
2022-05-11 21:58:11 -0500

    fix(clients): change URI name to PATH

--- src/main/java/com/pichincha/fabianOrdonez_inventarios/clients/web_client/ProductWebClient.java
+++ src/main/java/com/pichincha/fabianOrdonez_inventarios/clients/web_client/ProductWebClient.java
@@ -5,18 +5,17 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 public class ProductWebClient {
 
-  private static final String URI = "/v1/9d24a44e-1447-43c3-8651-5b8f6b86ea3c";
+  private static final String PATH = "/v1/9d24a44e-1447-43c3-8651-5b8f6b86ea3c";
   private static final String BASE_URL = "https://mocki.io";
 
   private final WebClient webClient = WebClient.create(BASE_URL);
 
   public List<ProductDto> retrieveProducts() {
-    return webClient.get().uri(URI)
+    return webClient.get().uri(PATH)
         .retrieve()
         .bodyToFlux(ProductDto.class)
         .collectList()
