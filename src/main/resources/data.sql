bc5aecc3dc40f2a93f69c4393d7829d430f9b01c
Fabian Ordonez <fabian.ordonez@devsu.com>
2022-05-11 19:26:52 -0500

    feat(sql): add sql script to fill shops table

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO `shops` (`name`, `code`) VALUES ('east shop', 'shop-01');
+INSERT INTO `shops` (`name`, `code`) VALUES ('west shop', 'shop-02');
+INSERT INTO `shops` (`name`, `code`) VALUES ('south shop', 'shop-03');
+INSERT INTO `shops` (`name`, `code`) VALUES ('north shop', 'shop-04');
